21c641c00c3eeb57f1ab879d814b2278e8c003aa
crunchysteve <46626696+crunchysteve@users.noreply.github.com>
2021-02-26 19:31:41 +1100

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -14,18 +14,3 @@ switcher.addEventListener('click', function() {
     }
     console.log('current class name: ' + className);
 });
-/*
-  function loadFile(filePath) {
-    var result = null;
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", filePath, false);
-    xmlhttp.send();
-    if (xmlhttp.status==200) {
-      result = xmlhttp.responseText;
-    }
-    return result;
-  }
-    
-  var myStuff = loadFile("posts/hello.txt");
-  document.getElementById("out").innerHTML = file;
-*/
